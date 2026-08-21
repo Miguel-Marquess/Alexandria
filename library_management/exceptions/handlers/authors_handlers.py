101c59e739f77f05a93ef4e2bdad16a63329c568
Miguel-Marquess <mluiz4784@gmail.com>
2026-08-21 10:32:47 -0300

    refactor: add exception handler for authors excs.

--- library_management/exceptions/handlers/authors_handlers.py
+++ library_management/exceptions/handlers/authors_handlers.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-from library_management.exceptions.authors_exceptions import AuthorNotFound
+from library_management.exceptions.authors_exceptions import (
+    AuthorHasRegisteredBooks,
+    AuthorNone,
+    AuthorNotFound,
+)
 
 handler = FastAPI()
 
@@ -13,6 +17,22 @@ async def author_not_found_handler(req: Request, exc: AuthorNotFound):
     )
 
 
+@handler.exception_handler(AuthorNone)
+async def author_cannot_none(req: Request, exc: AuthorNone):
+    return JSONResponse(status_code=422, content='Author name cannot be None.')
+
+
+@handler.exception_handler(AuthorHasRegisteredBooks)
+async def author_has_registed_books(req: Request, exc: AuthorHasRegisteredBooks):
+    return JSONResponse(
+        status_code=409,
+        content=f'Author (ID [{exc.author_id}]) has registered books. '
+        'If you want continue, delete the authors books.',
+    )
+
+
 author_exc_handlers = {
     AuthorNotFound: author_not_found_handler,
+    AuthorNone: author_cannot_none,
+    AuthorHasRegisteredBooks: author_has_registed_books,
 }
